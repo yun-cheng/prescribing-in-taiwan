3e9c9e21ae5039531a6ee0d1dab27792ecc19500
yun-cheng <yuncheng.dev@gmail.com>
2024-01-10 14:27:52 +0800

    feat: open SideBar on chart title click

--- src/components/chart/ChartHeader.tsx
+++ src/components/chart/ChartHeader.tsx
@@ -1,14 +1,27 @@
 'use client';
 
-import { selectedDrugAtom } from '@/atoms/sideBar';
-import { useAtom } from 'jotai';
+import { selectedDrugAtom, sideBarOpenAtom } from '@/atoms/sideBar';
+import { ExpandMore } from '@mui/icons-material';
+import { useAtom, useSetAtom } from 'jotai';
 
 export default function ChartHeader() {
   const [drug] = useAtom(selectedDrugAtom);
+  const toggleSideBar = useSetAtom(sideBarOpenAtom);
+
+  const handleClickTitle = () => {
+    toggleSideBar(true);
+  };
 
   return (
     <div className="mx-auto mb-4 max-w-screen-lg text-center">
-      <h2 className="text-2xl font-semibold">{drug.label}</h2>
+      <button
+        type="button"
+        className="text-2xl font-semibold"
+        onClick={handleClickTitle}
+      >
+        {drug.label}
+        <ExpandMore fontSize="inherit" />
+      </button>
       <p className="text-lg">{drug.note}</p>
     </div>
   );
