8a8e69718ef5d7ddaadb95352929380fc7e5912d
yun-cheng <yuncheng.dev@gmail.com>
2024-01-11 16:50:09 +0800

    refactor: replace MUI styled component with Tailwind in MainContainer

--- src/components/common/MainContainer.tsx
+++ src/components/common/MainContainer.tsx
@@ -1,29 +1,9 @@
 'use client';
 
 import { sideBarOpenAtom } from '@/atoms/sideBar';
-import { styled, useMediaQuery, useTheme } from '@mui/material';
+import { useMediaQuery, useTheme } from '@mui/material';
 import { useAtom } from 'jotai';
 import { drawerWidth } from './SideBar/constants';
-
-const Main = styled('div', {
-  shouldForwardProp: (prop) => prop !== 'open',
-})<{
-  open?: boolean;
-}>(({ theme, open }) => ({
-  flexGrow: 1,
-  transition: theme.transitions.create('margin', {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${drawerWidth}px`,
-  ...(open && {
-    transition: theme.transitions.create('margin', {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
-}));
 
 type Props = {
   children: React.ReactNode;
@@ -35,9 +15,21 @@ export default function MainContainer({ children }: Props) {
 
   const [sideBarOpen] = useAtom(sideBarOpenAtom);
 
-  if (lgUp) {
-    return <Main open={sideBarOpen}>{children}</Main>;
-  }
+  const marginLeft = sideBarOpen || !lgUp ? 0 : -drawerWidth;
 
-  return <div className="w-full">{children}</div>;
+  const transitionOpen = theme.transitions.create('margin', {
+    easing: theme.transitions.easing.easeOut,
+    duration: theme.transitions.duration.leavingScreen,
+  });
+  const transitionClose = theme.transitions.create('margin', {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  });
+  const transition = sideBarOpen ? transitionOpen : transitionClose;
+
+  return (
+    <div className="flex-grow" style={{ marginLeft, transition }}>
+      {children}
+    </div>
+  );
 }
