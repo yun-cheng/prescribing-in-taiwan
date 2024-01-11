8944ab014a2ec2c65fc3510c57723b3661ef283f
yun-cheng <yuncheng.dev@gmail.com>
2024-01-11 21:17:05 +0800

    refactor: replace styled AppBar with sx prop and use sx breakpoints when possible

--- src/components/common/HideOnScroll.tsx
+++ src/components/common/HideOnScroll.tsx
@@ -4,13 +4,18 @@ import Slide from '@mui/material/Slide';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 
 interface Props {
+  disable?: boolean;
   children: React.ReactElement;
 }
 
-export default function HideOnScroll({ children }: Props) {
+export default function HideOnScroll({ disable = false, children }: Props) {
   const trigger = useScrollTrigger({
     threshold: 30,
   });
+
+  if (disable) {
+    return children;
+  }
 
   return (
     <Slide appear={false} direction="down" in={!trigger}>
--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -4,44 +4,21 @@ import { sideBarOpenAtom } from '@/atoms/sideBar';
 import MenuIcon from '@mui/icons-material/Menu';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import {
+  AppBar,
   Button,
   IconButton,
   Link,
   Toolbar,
-  styled,
   useMediaQuery,
   useTheme,
 } from '@mui/material';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
 import { useAtom } from 'jotai';
 import HideOnScroll from './HideOnScroll';
 import { drawerWidth } from './SideBar/constants';
 
-interface AppBarProps extends MuiAppBarProps {
-  move?: boolean;
-}
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'move',
-})<AppBarProps>(({ theme, move }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(move && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
 export default function NavBar() {
   const theme = useTheme();
   const mdUp = useMediaQuery(theme.breakpoints.up('md'));
-  const lgUp = useMediaQuery(theme.breakpoints.up('lg'));
 
   const [sideBarOpen, toggleSideBar] = useAtom(sideBarOpenAtom);
 
@@ -49,44 +26,68 @@ export default function NavBar() {
     toggleSideBar();
   };
 
-  const appBar = (
-    <AppBar move={lgUp && sideBarOpen} sx={{ backgroundColor: '#475569' }}>
-      <div className="mx-auto w-full max-w-screen-lg">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            edge="start"
-            onClick={handleClickMenu}
-            sx={{ mr: 2, ...(lgUp && sideBarOpen && { display: 'none' }) }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Link
-            href="/"
-            color="inherit"
-            underline="none"
-            sx={{ fontSize: '1.5rem', fontWeight: 600 }}
-          >
-            Prescribing in Taiwan
-          </Link>
-          <div className="flex-grow" />
-          <Button
-            href="https://www.pleiotropy.co.uk/prescribing-in-taiwan"
-            target="_blank"
-            rel="noopener noreferrer"
-            color="inherit"
-            sx={{
-              fontSize: '1.125rem',
-              display: { xs: 'none', sm: 'block' },
-            }}
-          >
-            More Info
-            <OpenInNewIcon className="ml-1" />
-          </Button>
-        </Toolbar>
-      </div>
-    </AppBar>
-  );
+  const transitionOpen = theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.easeOut,
+    duration: theme.transitions.duration.enteringScreen,
+  });
+  const transitionClose = theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  });
+  const transition = sideBarOpen ? transitionOpen : transitionClose;
 
-  return <div>{!mdUp ? <HideOnScroll>{appBar}</HideOnScroll> : appBar}</div>;
+  return (
+    <HideOnScroll disable={mdUp}>
+      <AppBar
+        sx={{
+          backgroundColor: '#475569',
+          transition,
+          [theme.breakpoints.up('lg')]: {
+            marginLeft: sideBarOpen ? drawerWidth : 0,
+            width: sideBarOpen ? `calc(100% - ${drawerWidth}px)` : '100%',
+          },
+        }}
+      >
+        <div className="mx-auto w-full max-w-screen-lg">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              edge="start"
+              onClick={handleClickMenu}
+              sx={{
+                mr: 2,
+                display: {
+                  lg: sideBarOpen ? 'none' : 'block',
+                },
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Link
+              href="/"
+              color="inherit"
+              underline="none"
+              sx={{ fontSize: '1.5rem', fontWeight: 600 }}
+            >
+              Prescribing in Taiwan
+            </Link>
+            <div className="flex-grow" />
+            <Button
+              href="https://www.pleiotropy.co.uk/prescribing-in-taiwan"
+              target="_blank"
+              rel="noopener noreferrer"
+              color="inherit"
+              sx={{
+                fontSize: '1.125rem',
+                display: { xs: 'none', sm: 'block' },
+              }}
+            >
+              More Info
+              <OpenInNewIcon className="ml-1" />
+            </Button>
+          </Toolbar>
+        </div>
+      </AppBar>
+    </HideOnScroll>
+  );
 }
